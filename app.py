da3ce308a6086db42158c12dbfc428a902be1a8f
Ruta <rutalathiya09@gmail.com>
2026-05-13 21:00:20 +0530

    simple insertion and fetch operation

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "Flask is Working!"
+
+    # Save task
+    if request.method == "POST":
+
+        task = request.form["task"]
+
+        conn = sqlite3.connect("tasks.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO tasks (title) VALUES (?)",
+            (task,)
+        )
+
+        conn.commit()
+        conn.close()
+
+    # Fetch tasks from database
+    conn = sqlite3.connect("tasks.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM tasks")
+
+    tasks = cursor.fetchall()
+
+    conn.close()
+
+    return render_template("index.html", tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect("tasks.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    completed INTEGER DEFAULT 0
+)
+""")
+
+conn.commit()
+
+conn.close()
+
+print("Database created successfully!")
